06680ddc865aef1faea51fd1c8107ba59d805e65
Sean <spl48@uclive.ac.nz>
2019-03-17 13:25:27 +1300

    Implemented check if email is valid format

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -1,13 +1,26 @@
 const  User  =  require ( '../models/user.server.model' );
 exports . create  =  function ( req , res ){
 
+    //Check if username is empty
     if (req.body.username == null || req.body.username == "") {
         res.status(400).send({error: 'Bad Request'});
         return;
     }
 
+    //Check if email is empty
     if (req.body.email == null || req.body.email == "") {
         res.status(400).send({error: 'Bad Request'});
+        return;
+    }
+
+    //Check if email is valid
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    if (!validateEmail(req.body.email)) {
+        res.status(400).send({error: 'Bad email'});
         return;
     }
 
