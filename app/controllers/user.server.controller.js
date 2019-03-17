362ad5195f59af378b7f8fd02c292122e2e0d353
Sean <spl48@uclive.ac.nz>
2019-03-17 13:47:08 +1300

    Triple ===

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -2,13 +2,13 @@ const  User  =  require ( '../models/user.server.model' );
 exports . create  =  function ( req , res ){
 
     //Check if username is empty
-    if (req.body.username === null || req.body.username === "") {
+    if (req.body.username == null || req.body.username === "") {
         res.status(400).send({error: 'Bad Request'});
         return;
     }
 
     //Check if email is empty
-    if (req.body.email === null || req.body.email === "") {
+    if (req.body.email == null || req.body.email === "") {
         res.status(400).send({error: 'Bad Request'});
         return;
     }
