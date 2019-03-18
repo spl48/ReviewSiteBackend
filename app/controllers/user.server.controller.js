e40fe402a5dc4301f9bacc9d88ed7b149483dfdc
Sean <spl48@uclive.ac.nz>
2019-03-18 22:23:00 +1300

    Implemented GET users/id

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -165,31 +165,31 @@ exports . getUserInfo = function (req , res) {
         userID, authToken
     ];
 
-    // User.authorize(authToken, function (result) {
-    //     if (result.length !== 0) {
-    //         authorized = true;
-    //         requestedUser = result[0]['user_id'];
-    //     } else {
-    //         res.status(404).send({error: 'Not Found'});
-    //     }
-    // });
+    User.authorize(authToken, function (result) {
+        if (result.length !== 0) {
+            authorized = true;
+            requestedUser = result[0]['user_id'];
+        } else {
+            res.status(404).send({error: 'Not Found'});
+        }
+    });
 
-    // User.getUser(values, function (result) {
-    //     if (authorized && userID.toString() === requestedUser.toString()) {
-    //         res.status(200);
-    //         res.json({
-    //             username: result[0]['username'],
-    //             email: result[0]['email'],
-    //             givenName: result[0]['given_name'],
-    //             familyName: result[0]['family_name']
-    //         });
-    //     } else if (authorized && userID.toString() !== requestedUser.toString()) {
-    //         res.status(200);
-    //         res.json({
-    //             username: result[0]['username'],
-    //             givenName: result[0]['given_name'],
-    //             familyName: result[0]['family_name']
-    //         });
-    //     }
-    // })
+    User.getUser(values, function (result) {
+        if (authorized && userID.toString() === requestedUser.toString()) {
+            res.status(200);
+            res.json({
+                username: result[0]['username'],
+                email: result[0]['email'],
+                givenName: result[0]['given_name'],
+                familyName: result[0]['family_name']
+            });
+        } else if (authorized && userID.toString() !== requestedUser.toString()) {
+            res.status(200);
+            res.json({
+                username: result[0]['username'],
+                givenName: result[0]['given_name'],
+                familyName: result[0]['family_name']
+            });
+        }
+    })
 };
