d333694490d260bbe2cb5ac8e8a430a3a7811cef
Sean <spl48@uclive.ac.nz>
2019-03-20 14:07:16 +1300

    Implemented GET users/id asynchrously

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -19,7 +19,7 @@ exports . verify = function(values, authToken, done){
 
 exports . authorize = async function (authToken) {
     try {
-        let result = await db.getPool().query('SELECT user_id FROM User WHERE auth_token=?', authToken.toString());
+        let result = await db.getPool().query('SELECT user_id FROM User WHERE auth_token=?', authToken);
         return result;
     } catch (err) {
         throw (err);
