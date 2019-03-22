ee68434ffa5931041b754ecd78430a09cc6c4748
Sean <spl48@uclive.ac.nz>
2019-03-23 12:00:49 +1300

    Implemented GET /venues/:id/photos/:photoFilename

--- app/controllers/venues.photos.server.controller.js
+++ app/controllers/venues.photos.server.controller.js
@@ -211,6 +211,17 @@ exports . retrieve = async function (req , res) {
 
     console.log(requestedVenueId);
     console.log(filename);
+    fs.readdir(base, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file);
+        });
+    });
 
     let dbFilename = null;
     try {
